0811c62a502b11407c371b018011d10e5e66fe49
Diederik <djh@skynamo.com>
2022-08-13 18:04:00 +0200

    Added embedded flutter fragment.

--- app/src/main/java/guru/diederik/scrollviewsanitycheck/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/guru/diederik/scrollviewsanitycheck/ui/dashboard/DashboardFragment.java
@@ -7,13 +7,22 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
+import guru.diederik.scrollviewsanitycheck.R;
 import guru.diederik.scrollviewsanitycheck.databinding.FragmentDashboardBinding;
+import io.flutter.embedding.android.FlutterFragment;
+import io.flutter.embedding.android.TransparencyMode;
 
 public class DashboardFragment extends Fragment {
 
+    private static final String TAG_REP_GOALS_FLUTTER_FRAGMENT_1 = "FLUTTER_FRAGMENT_TAG_1";
+    private static final String TAG_REP_GOALS_FLUTTER_FRAGMENT_2 = "FLUTTER_FRAGMENT_TAG_2";
+
     private FragmentDashboardBinding binding;
+    private FlutterFragment mRepGoalsFlutterFragment1;
+    private FlutterFragment mRepGoalsFlutterFragment2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,8 +38,44 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        setupFlutterFragment();
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+    private void setupFlutterFragment(){
+        FragmentManager fragmentManager = getChildFragmentManager();
+
+        if (mRepGoalsFlutterFragment1 == null) {
+            mRepGoalsFlutterFragment1 = FlutterFragment.withNewEngine()
+//                    .transparencyMode(TransparencyMode.opaque)
+                    .build();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.cardview7,
+                            mRepGoalsFlutterFragment1,
+                            TAG_REP_GOALS_FLUTTER_FRAGMENT_1)
+                    .commit();
+        }
+
+        if (mRepGoalsFlutterFragment2 == null) {
+            mRepGoalsFlutterFragment2 = FlutterFragment.withNewEngine()
+//                    .transparencyMode(TransparencyMode.opaque)
+                    .build();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.cardView,
+                            mRepGoalsFlutterFragment2,
+                            TAG_REP_GOALS_FLUTTER_FRAGMENT_2)
+                    .commit();
+        }
+
+    }
+
 }
